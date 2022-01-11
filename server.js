3d92b4a335ae23cfaad06007f32320ca14bde171
Kyle Johnston <kyle9657@gmail.com>
2022-01-11 12:12:57 +0000

    Updated dev creds

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
 
 const app = express();
 const port = 3000;
 
-
-
 const con = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "dev-user",
   password: "Password1",
-  port: '3306'
+  port: "3306",
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("Connection to MySQL DB successful");
 });
 
-app.listen(port, () => {            
-    //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
+app.listen(port, () => {
+  //server starts listening for any attempts from a client to connect at port: {port}
+  console.log(`Now listening on port ${port}`);
 });
